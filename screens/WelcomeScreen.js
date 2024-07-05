aede883fb5b8d51e5b8be04132ed6e22adc8ccde
Frank Su <frank.su@applydigital.co>
2024-07-05 13:15:17 -0700

    Add access to authenticated database data and logout button

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [fetchMessage, setFetchMessage] = useState("");
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+  useEffect(() => {
+    axios
+      .get(
+        "https://udemy-rn-2522a-default-rtdb.firebaseio.com/message.json?auth=" +
+          token
+      )
+      .then((response) => {
+        setFetchMessage(response.data);
+      });
+  }, [token]);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchMessage}</Text>
     </View>
   );
 }
@@ -14,13 +31,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
